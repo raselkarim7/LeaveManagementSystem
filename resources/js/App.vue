738bec093a73381a79145bbc6ac6d851b56e9416
raselkarim7 <rtc.raselkarim7@gmail.com>
2020-04-28 02:18:21 +0600

    bootstrap and vue scaffolding installed

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <main>
+        <h2>Leave Management system</h2>
+        <button class="btn btn-primary"> Test Btn</button>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+import Vue  from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
