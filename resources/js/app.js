c14ce7d1a8ee6169172f3a2fe10c84328b3c1819
raselkarim7 <rtc.raselkarim7@gmail.com>
2020-04-28 14:16:26 +0600

    Test component form made

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import Vue  from 'vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+
 
 import router from './router.js'; 
 import App from './App.vue';
--- resources/js/views/Test.vue
+++ resources/js/views/Test.vue
@@ -7,33 +7,92 @@
 
 
         <div class="card mb-4">
-            <div class="card-header"><i class="fas fa-table mr-1"></i>Test View</div>
+            <div class="card-header d-flex">
+							<span>
+							<i class="fas fa-table mr-1"></i>Test View
+							</span>
+
+							<button class="btn btn-primary btn-sm ml-auto" @click="showTestModal">
+								<span class="fa fa-plus"></span>	Create
+							</button>
+						
+						</div>
             <div class="card-body">
                 <table class="table">
-									<thead>
-										<tr>
-											<th scope="col">#</th>
-											<th scope="col">First</th>
-											<th scope="col">Last</th>
-											<th scope="col">Handle</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<th scope="row">1</th>
-											<td>Mark</td>
-											<td>Otto</td>
-											<td>@mdo</td>
-										</tr>
-									</tbody>
-								</table>
+					<thead>
+						<tr>
+							<th scope="col">#</th>
+							<th scope="col">First</th>
+							<th scope="col">Last</th>
+							<th scope="col">Handle</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">1</th>
+							<td>Mark</td>
+							<td>Otto</td>
+							<td>@mdo</td>
+						</tr>
+					</tbody>
+				</table>
             </div>
         </div>
+
+				<b-modal ref="myTestModal" hide-footer title="Add New Test">
+					<div class="d-block">
+						<form > 
+							<div class="form-group">
+								<label for="name">Email address</label>
+								<input 
+									v-model="test.name"
+									type="text" class="form-control" id="name" placeholder="Enter name here"
+								>
+							</div>
+							<div class="form-group">
+								<label for="image">Choose an image</label>
+								<input 
+									type="file" class="form-control" id="image" 
+									v-on:change="attachImage"
+								>
+							</div>
+						</form>
+
+
+					</div>
+					<button type="button" class="btn btn-default" @click="hideTestModal">Cancel</button>
+					<button type="button" @click="createNewTest" class="btn btn-primary">
+						<span class="fa fa-check"></span>	Save
+					</button>
+				</b-modal>
+
     </div>
 </template>
 
 <script>
 export default {
-    name: 'TestView'
+		name: 'TestView', 
+		data() {
+			return {
+				test: {
+					name: '', 
+					image: ''
+				}
+			}
+		}, 
+		methods: {
+			hideTestModal() {
+				this.$refs.myTestModal.hide()
+			},
+			showTestModal() {
+				this.$refs.myTestModal.show()
+			},  
+			attachImage() {
+
+			}, 
+			createNewTest() {
+				console.log('form submit works')
+			}
+		}
 }
 </script>
