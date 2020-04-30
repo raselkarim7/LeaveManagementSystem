dcb33663861d683247451bd2e50eef4133f4e71e
raselkarim7 <rtc.raselkarim7@gmail.com>
2020-04-30 20:05:46 +0600

    footer page unnecessary html removed

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -55,8 +55,8 @@
                 </div>
             </div>
             <div class="sb-sidenav-footer">
-                <div class="small">Logged in as:</div>
-                Start Bootstrap
+                <!-- <div class="small">Logged in as:</div>
+                Start Bootstrap -->
             </div>
         </nav>
     </div>
