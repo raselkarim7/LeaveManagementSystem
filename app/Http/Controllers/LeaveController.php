67f2dea903503ed697a522eeb7c1d9785a8405f4
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-01-25 12:27:24 +0600

    Remove unused code

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -33,16 +33,6 @@ class LeaveController extends Controller
         //
     }
 
-    // /**
-    //  * Show the form for creating a new resource.
-    //  *
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function create()
-    // {
-    //     //
-    // }
-
     /**
      * Store a newly created resource in storage.
      *
